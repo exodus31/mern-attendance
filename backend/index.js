4b0b9a23e614e0208c961d4c5dd8679d35a97018
exodus31 <sarthak.nandre31@gmail.com>
2023-05-11 00:23:11 +0530

    yth

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import jwt from 'jsonwebtoken';
 import cors from 'cors';
 
 const app = express();
-var port =  process.env.PORT ;
+var port =  process.env.PORT || 3000 ;
 app.use(cors());
 app.use(express.json());
 app.use("/", (req, res) => {
