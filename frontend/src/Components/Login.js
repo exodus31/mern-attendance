c4c07f7a22e563c5dea162a55d4ebb9fa43a7f74
exodus31 <sarthak.nandre31@gmail.com>
2023-06-28 23:08:15 -0700

    few

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -23,7 +23,7 @@ function Login(props) {
         const token = res.data.token;
         localStorage.setItem("token", token);
           
-        window.location.href = "/home";
+        window.location.href = "https://main--genuine-froyo-43a41c.netlify.app/home";
       } else {
         console.log(res.data)     
       }   
